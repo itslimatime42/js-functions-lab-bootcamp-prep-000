93f07b46ac72ac6d057bc71f9d4df90b64c60892
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 03:35:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
+function holidayCountdown(holiday, number) {
+  return `It's ${number} days until ${holiday}`
+}
